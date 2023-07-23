23550e66135ed89b77f8412ce3d48c753eca66bd
YKIM <dyounggkim@gmail.com>
2023-07-23 20:34:18 +0100

    front page

--- src/pages/Landing Page/About.jsx
+++ src/pages/Landing Page/About.jsx
@@ -1,6 +1,13 @@
+import classes from "./About.module.css";
+
 export default function About() {
 
-  return <div>
-    About page
+  return <div className={classes.about_wrapper}>
+
+    <h2>How to use this cloud platform</h2>
+    <p>This is an cloud platform inspired by Heroku that I have built for web developers to deploy their applications easier like Heroku.</p>
+
+    <p>This cloud platform is built on the infrastructure of Amazon Web Services to provide free compute hours and additional features such as relational databases and networking services.</p>
+
   </div>
 }
--- src/pages/Landing Page/ExtraFeatures.jsx
+++ src/pages/Landing Page/ExtraFeatures.jsx
@@ -1,6 +1,9 @@
+import classes from "./ExtraFeatures.module.css";
+
 export default function ExtraFeatures() {
 
-  return <div>
-    Extra features
+  return <div className={classes.about_wrapper}>
+    <h2>Extra Features Available</h2>
+    <p>This cloud platform also provides Amazon RDS, DynamoDB, and some network servies.</p>
   </div>
 }

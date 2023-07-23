e4e65e51339e82c637b7b49416534bf70be929fb
YKIM <dyounggkim@gmail.com>
2023-07-23 21:07:46 +0100

    front page css/structure

--- src/pages/Landing Page/About.jsx
+++ src/pages/Landing Page/About.jsx
@@ -6,7 +6,7 @@ export default function About() {
   
   <div className={classes.about_wrapper}>
 
-    <h2>How to use this cloud platform</h2>
+    <h2>What this cloud platform is about</h2>
     <p>This is an cloud platform inspired by Heroku that I have built for web developers to deploy their applications easier like Heroku.</p>
 
     <p>This cloud platform is built on the infrastructure of Amazon Web Services to provide free compute hours and additional features such as relational databases and networking services.</p>
